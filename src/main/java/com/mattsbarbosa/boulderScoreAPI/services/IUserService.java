b029f8361b930e6877dae59cc7d2f4305ea263db
MattsBarbosa <matts.barbosa@gmail.com>
2024-10-29 20:09:09 -0300

    Deletando importações não utilizadas

--- src/main/java/com/mattsbarbosa/boulderScoreAPI/services/IUserService.java
+++ src/main/java/com/mattsbarbosa/boulderScoreAPI/services/IUserService.java
@@ -1,11 +1,10 @@
 package com.mattsbarbosa.boulderScoreAPI.services;
 
 import com.mattsbarbosa.boulderScoreAPI.dtos.UserDTO;
-import org.apache.tomcat.websocket.AuthenticationException;
 
 public interface IUserService {
 
-    public UserDTO register(UserDTO userDTO);
+    UserDTO register(UserDTO userDTO);
 
-    public String verify(UserDTO userDTO);
+    String verify(UserDTO userDTO);
 }
